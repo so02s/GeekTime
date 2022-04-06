13684c5d004b3b2b30ec65b68d4359aaff989c5e
so02s <102208102+so02s@users.noreply.github.com>
2022-04-06 09:32:55 +0300

    some js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,28 +1,26 @@
-﻿
-//проезжание вниз до нужного блока для вкладки интерьер
+﻿//проезжание вниз до нужного блока для вкладки интерьер
 $('.flowing-scroll').on('click', function () {
     var el = $(this);
     var dest = el.attr('href'); // получаем направление
     if (dest !== undefined && dest !== '') { // проверяем существование
         $('html').animate({
             scrollTop: $(dest).offset().top // прокручиваем страницу к требуемому элементу
-        }, 20 // скорость прокрутки
+        }, '20' // скорость прокрутки
         );
     }
     return false;
 });
 
-
 //меню
-<script>
-    jQuery(document).ready(function($){
-        // Клик по кнопке открывает меню, повторный клик закрывает  
-        $('.pushmenu').click(function () {
-            $('.pushmenu').toggleClass("open");
-            $('.sidebar').toggleClass("show");
-            $('.hidden-overley').toggleClass("show");
-            $('body').toggleClass("sidebar-opened")
-        });
+$(document).ready(function () {
+    // Клик по кнопке открывает меню, повторный клик закрывает  
+    $('.pushmenu').click(function () {
+        $('.pushmenu').toggleClass("open");
+        $('.sidebar').toggleClass("show");
+        $('.hidden-overley').toggleClass("show");
+        $('body').toggleClass("sidebar-opened")
+    });
+
     // Когда панель открыта, клик по области вне панели закрывает ее
     $('.hidden-overley').click(function(){
         $(this).toggleClass("show");
@@ -30,14 +28,15 @@ $('.flowing-scroll').on('click', function () {
       $('.pushmenu').toggleClass("open");
       $('body').toggleClass("sidebar-opened")
     });
-    // меняем активность пункта меню по клику (НЕОБЯЗАТЕЛЬНО)
+
+    // Меняем активность пункта меню по клику
     $('.sidebar ul li').click(function(){
         $(this).addClass("current-menu-item").siblings().removeClass("current-menu-item");
     });
+
     // Для анимации поворота каретки
     $('.menu-parent-item a:first-child').click(function(){
         $(this).siblings().toggleClass("show");
         $(this).find("i").toggleClass("rotate");
-    });
+       });
 });
-</script>
