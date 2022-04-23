3e313f2c4b4a398762025825033649354f363ff4
whiskolla <bfoyb19@gmail.com>
2022-04-23 13:54:57 +0300

    первая база данных под админа

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GeekTime.Manager;
+using GeekTime.Models;
+
+namespace GeekTime.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IAdminManager _adminManager;
+        public AdminController(IAdminManager adminManager)
+        {
+            _adminManager = adminManager;
+        }
+        public ViewResult AdminPage()
+        {
+            var students = _adminManager.admin;
+            return View(students);
+        }
+        public ViewResult CreateAdmin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(string Name, string Events, string Image)
+        {
+            try
+            {
+                _adminManager.AddAdmin(new Admin(Name, Events, Image));
+                return RedirectToAction(nameof(AdminPage));
+            }
+            catch
+            {
+                return RedirectToAction(nameof(AdminPage));
+            }
+        }
+    }
+}
+
