6a74a7bbb3621937173f23f2ea10996379e26b46
so02s <102208102+so02s@users.noreply.github.com>
2022-05-31 18:10:58 +0300

    ОНО РАБОТАЕТ

--- Site_Data/GeekTimeContext.cs
+++ Site_Data/GeekTimeContext.cs
@@ -7,15 +7,17 @@ namespace GeekTime.Site_Data
     public class GeekTimeContext : DbContext
     {
         public DbSet<Admin> Admins { get; set; }
+        /*
         public DbSet<Events> Events { get; set; }
         public DbSet<BoardGame> BoardGames { get; set; }
         public DbSet<Comics> Comics { get; set; }
         public DbSet<ConsoleGames> ConsoleGames { get; set; }
-        public DbSet<Contacts> Contacts { get; set; }
+        public DbSet<Contacts> Contacts { get; set; }О
         public DbSet<Post> Posts { get; set; }
         public DbSet<Rates> Rates { get; set; }
         public DbSet<Rooms> Rooms { get; set; }
         public DbSet<TimetableRent> TimetableRents { get; set; }
+        */
         public GeekTimeContext(DbContextOptions<GeekTimeContext> options) : base(options)
         {
             Database.EnsureCreated();
